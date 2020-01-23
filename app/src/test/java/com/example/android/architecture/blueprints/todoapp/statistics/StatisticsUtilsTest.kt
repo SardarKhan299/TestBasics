b99867d1ad29fe74df84d11a88827cc5f1afa86f
Administrator <sardar@mypaperlessoffice.com>
2020-01-24 02:10:53 +0500

    Add test cases for two methods to test the logic of the method.

---
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
@@ -0,0 +1,42 @@
+package com.example.android.architecture.blueprints.todoapp.statistics
+
+import android.util.Log
+import com.example.android.architecture.blueprints.todoapp.data.Task
+import org.junit.After
+import org.junit.Before
+
+import org.junit.Assert.*
+import org.junit.Test
+
+class StatisticsUtilsTest {
+
+    @Before
+    fun setUp() {
+    }
+
+    @After
+    fun tearDown() {
+    }
+    @Test
+    fun getActiveAndCompletedStats_noCompleted_returnsHundredZero(){
+        val tasks = listOf<Task>(Task("title1","Description1",false))
+        val result = getActiveAndCompletedStats(tasks)
+        assertEquals(0f,result.completedTasksPercent)
+        assertEquals(100f,result.activeTasksPercent)
+    }
+
+    @Test
+    fun getActiveAndCompletedStats_2Completed_3Active(){
+        val tasks = listOf<Task>(
+                Task("title1","Description1",true),
+                Task("title1","Description1",true),
+                Task("title1","Description1",false),
+                Task("title1","Description1",false),
+                Task("title1","Description1",false)
+                )
+        val result = getActiveAndCompletedStats(tasks)
+        assertEquals(40f,result.completedTasksPercent)
+        assertEquals(60f,result.activeTasksPercent)
+    }
+
+}
