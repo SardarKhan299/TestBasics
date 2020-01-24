efddf66008abdfa55f2d6630b8c71140720eb524
Administrator <sardar@mypaperlessoffice.com>
2020-01-25 00:06:16 +0500

    Add null and empty case handle using testing.

--- app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
+++ app/src/test/java/com/example/android/architecture/blueprints/todoapp/statistics/StatisticsUtilsTest.kt
@@ -33,10 +33,27 @@ class StatisticsUtilsTest {
                 Task("title1","Description1",false),
                 Task("title1","Description1",false),
                 Task("title1","Description1",false)
-                )
+        )
         val result = getActiveAndCompletedStats(tasks)
         assertEquals(40f,result.completedTasksPercent)
         assertEquals(60f,result.activeTasksPercent)
+    }
+
+
+    @Test
+    fun getActiveAndCompletedStats_Empty(){
+        val tasks = emptyList<Task>()
+        val result = getActiveAndCompletedStats(tasks)
+        assertEquals(0f,result.completedTasksPercent)
+        assertEquals(0f,result.activeTasksPercent)
+    }
+
+    @Test
+    fun getActiveAndCompletedStats_Null(){
+        val tasks = null
+        val result = getActiveAndCompletedStats(tasks)
+        assertEquals(0f,result.completedTasksPercent)
+        assertEquals(0f,result.activeTasksPercent)
     }
 
 }
